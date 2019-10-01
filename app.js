4b504d680cd99938ec3568aadd773c051f281dea
Raphael Onofre <raphael.onofre@gmail.com>
2019-10-01 09:21:06 -0300

    add notes function

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-const chalk = require('chalk')
-const validator = require('validator')
-const getNotes = require('./notes')
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
 
-const msg = getNotes()
+//  Customize yargs version
+yargs.version("1.1.0");
 
-console.log(msg)
-console.log(validator.isURL('htps://mead.io'))
-console.log(chalk.green.bold.inverse('Success!'))
+//  Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+//  Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Remove the note");
+  }
+});
+
+//  Create list command
+yargs.command({
+  command: "list",
+  describe: "List all the notes",
+  handler: function() {
+    console.log("Listing all the notes");
+  }
+});
+
+//  Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading the note");
+  }
+});
+
+yargs.parse();
